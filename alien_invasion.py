36f9306104d0cf9af5c2474e627876a46f2882bb
funlee <funlee@gmail.com>
2018-04-03 23:42:36 +0800

    init screen

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+  '''初始化游戏并创建一个屏幕对象'''
+  pygame.init()
+  ai_settings = Settings()
+  screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+  pygame.display.set_caption('Alien Invasion')
+  ship = Ship(screen) # 创建一艘飞船
+
+  bg_color = (230,230,230)
+
+  '''开始游戏的主循环'''
+  while True:
+    gf.check_events()
+    '''让最近绘制的屏幕可见'''
+    gf.update_screen(ai_settings,screen,ship)
+
+run_game()
